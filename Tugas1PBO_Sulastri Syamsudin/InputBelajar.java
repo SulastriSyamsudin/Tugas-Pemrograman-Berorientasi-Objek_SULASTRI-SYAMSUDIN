f1180c93c3e21d6b70859c0b27dfa4415c44222c
SulastriSyamsudin <sulastrisyamsudin736@gmail.com>
2026-04-01 18:36:54 +0700

    Add files via upload

---
+++ Tugas1PBO_Sulastri Syamsudin/InputBelajar.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+public class InputBelajar {
+    public static void main(String[] args) {
+        // Input dari user
+        String belajar = JOptionPane.showInputDialog("Anda sedang belajar apa?");
+        
+        // Menampilkan pesan
+        JOptionPane.showMessageDialog(null, "Belajar " + belajar + " sangat mudah");
+    }
+}
